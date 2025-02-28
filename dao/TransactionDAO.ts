e18d996d585a3bc4527704bb206fdbda4f59c0da
Kavindu <gayanthakavindu3@gmail.com>
2025-02-28 09:53:49 +0530

    added transaction type

--- dao/TransactionDAO.ts
+++ dao/TransactionDAO.ts
@@ -1,10 +1,12 @@
 export default class TransactionDAO {
+    id: string
     category: string;
     type: TransactionType;
     amount: number;
     date: string;
 
-    constructor(category: string,type: TransactionType,amount: number,date: string) {
+    constructor(id: string,category: string,type: TransactionType,amount: number,date: string) {
+        this.id = id;
         this.category = category;
         this.type = type;
         this.amount = amount;
--- dto/TransactionDTO.ts
+++ dto/TransactionDTO.ts
@@ -1,10 +1,12 @@
 export default class TransactionDTO {
+    id?: string;
     category: string;
     type: string;
     amount: number;
     date: string;
 
-    constructor(category: string,type: string,amount: number,date: string) {
+    constructor(id: string,category: string,type: string,amount: number,date: string) {
+        this.id = id;
         this.category = category;
         this.type = type;
         this.amount = amount;
